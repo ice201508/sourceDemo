4403d62e5da3673263d1d7c6d912f91635ace1c3
ice201508 <2235907780@qq.com>
2016-10-13 18:55:44 +0800

    __proto__

--- ES5_ES6/commonJS/calculate.js
+++
@@ -1,6 +0,0 @@
-//引入外部模块
-
-var math= require('sum');
-exports add = function(n){
-	return math.sum(val, n);
-}
--- ES5_ES6/commonJS/sum.js
+++
@@ -1,6 +0,0 @@
-//CommonJS
-
-//exports 对象用于导出当前模块的方法或变量，唯一的导出口
-exports.sum = function(x, y){
-	return x+y;
-}
