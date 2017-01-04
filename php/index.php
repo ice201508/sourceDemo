bbda82fa186456ff46233ae00eaf3e0445b9b1ae
ice201508 <2235907780@qq.com>
2017-01-04 19:26:31 +0800

    抽象类absctract

--- php/index.php
+++ php/index.php
@@ -37,6 +37,7 @@ echo $google->url;
 
 //继承 extends
 class Child_Site extends Site {
+	const constant= '常量值';
 	public $category;
 
 	function getCategory(){
@@ -45,6 +46,7 @@ class Child_Site extends Site {
 
 	//方法的重写(override)，覆盖
 	function getUrl(){
+		echo self::constant. "<br>";
 		echo "继承重写的方法的：" . $this->url . "<br>";
 	}
 
@@ -57,6 +59,7 @@ $childSide = new Child_Site("ssss", "构造函数的继承");
 //$childSide->setUrl("www.baidu.com");
 $childSide->getUrl();
 
+echo Child_Site::constant;
 
 // 接口  定义interface 实现implements，指定某个类必须实现哪些方法
 interface iTemplate {
@@ -65,8 +68,7 @@ interface iTemplate {
 }
 
 class Template implements iTemplate {
-	const constant= "常量值";
-	
+
 	private $vars = array();
 
 	public function setVariable($name, $var){
@@ -81,4 +83,33 @@ class Template implements iTemplate {
 	}
 }
 
+
+//抽象类abstruct,  不能被实例化。任何一个类如果里面有一个方法被声明为抽象的，那么这个类必须被声明为抽象。
+abstract class AbstractClass {
+	const sss = "抽象类的常量";
+	abstract protected function getValue();
+	//被定义的抽象的方法只是声明的其调用方式(参数)，不能定义具体功能的实现
+	abstract protected function prefixValue($prefix);
+
+	public function printOut() {
+		print $this->getValue() . "<br>";
+	}
+}
+
+echo '<br>.'.AbstractClass::sss.'    sssssssssssss' . '<br>';
+
+//继承一个抽象类时，子类必须定义父类中所有的抽象方法
+class ConcreteClass1 extends AbstractClass
+{
+	protected function getValue() {
+		return "ConcreteClass1 类". "<br>";
+	}
+
+	public function prefixValue($prefix) {
+		return "{$prefix},  ConcreteClass1". "<br>";
+	}
+}
+
+$class1 = new ConcreteClass1;
+$class1->printOut();
 ?>
